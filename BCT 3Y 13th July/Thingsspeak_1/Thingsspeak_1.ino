c6430d67a4f51a1859b5d876ed384c0a0fdb0d3e
Tirtharaj Bhattacharya <sutirtho17@gmail.com>
2026-07-14 14:54:55 +0530

    Add files via upload

---
+++ BCT 3Y 13th July/Thingsspeak_1/Thingsspeak_1.ino
@@ -0,0 +1,65 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <DHT.h>
+
+const char* ssid = "moupia";
+const char* password = "abcdefgh";
+
+String apiKey = "CPQIXBZZYDDZ31W8";    
+const char* server = "http://api.thingspeak.com/update";
+
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected!");
+}
+
+void loop() {
+
+// -------- DHT11 --------
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+Serial.print("Temp: ");
+  Serial.print(temperature);
+  Serial.print("°C  | Humidity: ");
+  Serial.print(humidity);
+  Serial.println("%");
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    String url = server;
+    url += "?api_key=" + apiKey;
+    
+    url += "&field1=" + String(temperature);
+    url += "&field2=" + String(humidity);
+
+    http.begin(url);
+    int httpCode = http.GET();
+    http.end();
+
+    Serial.println("Data sent to ThingSpeak");
+  }
+
+  delay(15000);   // ThingSpeak minimum delay
+}
---
+++ BCT 3Y 13th July/Thingsspeak_MQ2/Thingsspeak_MQ2.ino
@@ -0,0 +1,78 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <DHT.h>
+
+const char* ssid = "Galaxy A12 A2AA";
+const char* password = "yapn8271";
+
+String apiKey = "3LRZNA5T4TPXGLDL";    
+const char* server = "http://api.thingspeak.com/update";
+
+#define MQ2_PIN 34
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected!");
+}
+
+void loop() {
+
+  // -------- MQ2 --------
+  int gasValue = analogRead(MQ2_PIN);
+
+ // Convert raw value (0–4095) to percentage (0–100)
+  int gasPercent = map(gasValue, 0, 4095, 0, 100);
+
+  // Limit between 0 and 100
+  gasPercent = constrain(gasPercent, 0, 100);
+
+
+  // -------- DHT11 --------
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Gas: ");
+  Serial.print(gasPercent);
+  Serial.print("%  | Temp: ");
+  Serial.print(temperature);
+  Serial.print("°C  | Humidity: ");
+  Serial.print(humidity);
+  Serial.println("%");
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    String url = server;
+    url += "?api_key=" + apiKey;
+    url += "&field1=" + String(gasPercent);
+    url += "&field2=" + String(temperature);
+    url += "&field3=" + String(humidity);
+
+    http.begin(url);
+    int httpCode = http.GET();
+    http.end();
+
+    Serial.println("Data sent to ThingSpeak");
+  }
+
+  delay(15000);   // ThingSpeak minimum delay
+}
